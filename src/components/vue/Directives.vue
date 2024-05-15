92ca2952694ad2d8a56c624ca4d94c217868d26c
JesusJK <camilojesus18@gmail.com>
2024-05-15 12:24:15 -0500

    opciones_formulario

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { date } from 'astro/zod';
+import { ref } from 'vue';
+
+let msg = ref ('soy tooltip');
+let checked = ref (false)
+
+let data = ["hola", "mundo", "cruel"]
+
+let filtered =data.filter(function (word){
+    return word.includes('o')
+})
+
+</script>
+
+<template>
+    <input 
+    class="border"
+    :class="{'border-orange-400' : checked}"
+    v-model="msg"
+    
+    >
+   <div  :title="msg">     <!--  v-bind: -->
+       {{ msg }}
+    </div>
+    <label for="checked">seleccionado {{ checked }}</label>
+    <input type="checkbox" v-model="checked">
+
+    <ul>
+        <li v-for="(text, index) in filtered" :key="index">
+        {{ text }}
+        </li>
+    </ul>
+</template>
---
+++ src/components/vue/TODO.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const tasks = ref([])
+const inputTask = ref('')
+
+function addtask(){
+    tasks.value.push(inputTask.value)
+    console.log(tasks)
+}
+
+
+</script>
+
+<template>
+    <div class="max-w-96 bg-slate-300 rounded shadow mx-auto mt-10 px-8 py-10">
+        <h1 class="text-lg text-center">Lista de Tareas</h1>
+        <div class="mt-4 mb-2">
+            <input v-model="inputTask" class="w-4/5" />
+            <button @click="addtask" class="w-1/5 bg-green-500 text-white font-bold">+</button>
+        </div>
+
+        <div 
+        v-for="(task,index) in tasks"
+        :key="index"
+         class="bg-white/60 px-2">{{task}}</div>
+    </div>
+</template>
